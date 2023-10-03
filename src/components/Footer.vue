92aa6a526b71ba395c24fbeb53e999245c052640
unknown <ahcene.zakaria29@gmail.com>
2023-10-03 21:23:47 +0200

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-footer >
+    <v-card flat tile class=" white--text py-12 px-5" width="100%" color="#F4F5F9">
+      <v-row>
+        <v-col cols="12" xs="12" sm="6" md="4" class="pa-16">
+          <div class="text-center">
+          <span >S</span><strong>hopio</strong>
+          </div>
+          <v-card-text class="mt-2">
+            <div class="text-center">
+                      Small plates, salads & sandwiches - an intimate setting
+                      with 12 indoor seats plus patio seating.
+                    </div>
+          </v-card-text>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="2">
+          <v-card-text class="white--text pt-0">
+            <h3>About</h3>
+          </v-card-text>
+          <v-card-text class="grey--text"> How it Work</v-card-text>
+          <v-card-text class="grey--text mt-n4"> Our Promrams </v-card-text>
+          <v-card-text class="grey--text mt-n4"> Promotions </v-card-text>
+          <v-card-text class="grey--text mt-n4"> Refer A Friend </v-card-text>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="2">
+          <v-card-text class="white--text pt-0">
+            <h3>Dellvery</h3>
+          </v-card-text>
+          <v-card-text class="grey--text"> Cost on Delevery </v-card-text>
+          <v-card-text class="grey--text mt-n4"> Payment Method </v-card-text>
+          <v-card-text class="grey--text mt-n4"> Delevery Astus </v-card-text>
+          <v-card-text class="grey--text mt-n4"> Returns </v-card-text>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="4">
+          <v-card-text class="white--text pt-0">
+            <h3>Contact</h3>
+          </v-card-text>
+          <v-card-text class="grey--text"> AAE IdeaPro </v-card-text>
+          <v-card-text class="grey--text mt-n4">
+            216 / C EAST Road
+          </v-card-text>
+          <v-card-text class="grey--text mt-n4"> aae.idapro.com </v-card-text>
+          <v-card-text class="grey--text mt-n4"> -B8254552214 </v-card-text>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+
+  }),
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app-bar flat>
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+
+    <v-app-bar-title>Shopio</v-app-bar-title>
+    <v-spacer></v-spacer>
+    <v-text-field
+      density="compact"
+      variant="solo"
+      label="Search products"
+      append-inner-icon="mdi-magnify"
+      single-line
+      hide-details
+      flat
+    ></v-text-field>
+    <v-spacer></v-spacer>
+
+    <v-btn prepend-icon="mdi mdi-download-circle-outline">
+      <template v-slot:prepend>
+        <v-icon color="success"></v-icon>
+      </template>
+
+      Download App
+    </v-btn>
+
+    <v-btn> Sign In / Login </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
